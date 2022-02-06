14f03b986ca6a695df77348c9f7a89a865021228
Sarang Sule <79126652+cFlour@users.noreply.github.com>
2022-02-06 23:34:03 +0530

    Create calendar.py

---
+++ calendar.py
@@ -0,0 +1,5 @@
+#date of any day of any year
+import calendar
+yy=int(input("Year? ")) #enter 4 digit year number
+mm=int(input("Month? ")) #enter two digit month number
+print(calendar.month(yy,mm)
