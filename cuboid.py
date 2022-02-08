cb0ada6eeb8bb3150137194bc3b4796210423ce6
Sarang Sule <79126652+cFlour@users.noreply.github.com>
2022-02-08 10:14:59 +0530

    Create cuboid.py

---
+++ cuboid.py
@@ -0,0 +1,7 @@
+#cuboid
+l=float(input("Length? "))
+b=float(input("Breadth? "))
+h=float(input("Height? "))
+v=l*b*h
+SA=2*((l*b)+(b*h)+(h*l))
+print("Volume of the cuboid is",v,"and surface area is",SA)
