63b4c020d0af887bcc2c295e4655f226faf51255
Sarang Sule <79126652+cFlour@users.noreply.github.com>
2022-02-08 08:50:41 +0530

    Create surface-area.py

---
+++ cylinder/surface-area.py
@@ -0,0 +1,10 @@
+#find the curved surface area of a cylinder
+r=float(input("Length of the radius? "))
+h=float(input("Height of the cylinder?"))
+pi=input("Value of pi? ")
+CSA=2*pi*r*h
+LSA=pi*r*r*2
+TSA=CSA+LSA
+print("CSA of the cylinder is",CSA)
+print("LSA of the cylinder is",LSA)
+print("TSA of the cylinder is",TSA)
