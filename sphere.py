00c8864344cf84e0299f74ad2cd40739af8f32fd
Sarang Sule <79126652+cFlour@users.noreply.github.com>
2022-02-08 09:00:33 +0530

    Create sphere.py

---
+++ sphere.py
@@ -0,0 +1,10 @@
+#inputs
+r=float(input("Length of the radius? "))
+pi=float(input("Value of pi? "))
+#surface area
+SA=4*pi*r*r
+#volume
+v=(4/3)*pi*r*r*r
+#output
+print("Surface area is",SA)
+print("Volume is",v)
