772114976963daa45b7c66f1f9d96f29c4c52812
Sarang Sule <79126652+cFlour@users.noreply.github.com>
2022-02-10 18:32:44 +0530

    Update volume.py

--- cylinder/volume.py
+++ cylinder/volume.py
@@ -1,5 +1,6 @@
+import math
 r=float(input("Length of the radius? "))
 h=float(input("Height of the cylinder? "))
-pi=22/7
-v=pi*r*r*h
+pi=math.pi
+v=round(pi*r*r*h,2)
 print("Volume of the cylinder is",v)
