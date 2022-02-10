759e78aaa224de1688ab4c04ea8429041bf2c40b
Sarang Sule <79126652+cFlour@users.noreply.github.com>
2022-02-10 18:33:56 +0530

    Add round function

--- cylinder/surface-area.py
+++ cylinder/surface-area.py
@@ -1,9 +1,10 @@
 #find the curved surface area of a cylinder
+import math
 r=float(input("Length of the radius? "))
 h=float(input("Height of the cylinder?"))
-pi=22/7
-CSA=2*pi*r*h
-LSA=pi*r*r*2
+pi=math.pi
+CSA=round(2*pi*r*h,2)
+LSA=round(2*pi*r*r,2)
 TSA=CSA+LSA
 print("CSA of the cylinder is",CSA)
 print("LSA of the cylinder is",LSA)
