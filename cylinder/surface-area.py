0ef2195d0b8b24d84ea346f2d70817ace95d3f10
Sarang Sule <79126652+cFlour@users.noreply.github.com>
2022-03-03 15:04:43 +0530

    Change from lateral to bottom

--- cylinder/surface-area.py
+++ cylinder/surface-area.py
@@ -4,8 +4,9 @@ r=float(input("Length of the radius? "))
 h=float(input("Height of the cylinder?"))
 pi=math.pi
 CSA=round(2*pi*r*h,2)
-LSA=round(2*pi*r*r,2)
+BSA=round(2*pi*r*r,2)
 TSA=CSA+LSA
 print("CSA of the cylinder is",CSA)
-print("LSA of the cylinder is",LSA)
+print("BSA of the cylinder is",BSA)
 print("TSA of the cylinder is",TSA)
+print("BSA=Bottom Surface Area")
