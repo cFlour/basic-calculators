1d6fb47ffe9a96f2e93ea4cada28b2145ebd1054
Sarang Sule <79126652+cFlour@users.noreply.github.com>
2022-10-29 22:02:11 +0530

    Seperate files for linear and quadratic
    Linear equation file

---
+++ linear-equations.py
@@ -0,0 +1,7 @@
+#solving linear and quadraticequations
+print("Let the equation be ax+b=c, where x is the variable. Enter the values:")
+a1=float(input("a="))
+b1=float(input("b="))
+c1=float(input("c="))
+y1=(c1-b1)/a1
+print("The value of the variable x is",y1)
